dc87ed659a366e8af3f8acadbfc01d52cab67d8d
00hongjun <00hongjun@gmail.com>
2021-06-09 22:44:57 +0900

    <feat> vacations 휴가 일급객체

--- src/main/java/com/sixshop/sixspace/vacation/repository/VacationRepository2.java
+++ src/main/java/com/sixshop/sixspace/vacation/repository/VacationRepository2.java
@@ -4,6 +4,7 @@ import static com.sixshop.sixspace.vacation.domain.QVacation.vacation;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sixshop.sixspace.vacation.domain.Vacation;
+import com.sixshop.sixspace.vacation.domain.Vacations;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,14 +16,16 @@ public class VacationRepository2 {
 
     private final JPAQueryFactory queryFactory;
 
-    public List<Vacation> findAllByBetweenDates(LocalDateTime start, final LocalDateTime end) {
-        return queryFactory
+    public Vacations findAllByBetweenDates(LocalDateTime start, final LocalDateTime end) {
+        List<Vacation> fetch = queryFactory
             .select(vacation)
             .from(vacation)
-            .where(vacation.startDateTime.between(start, end)
-                .or(vacation.endDateTime.between(start, end)))
-            .orderBy(vacation.startDateTime.asc())
+            .where(vacation.startDateTime.time.between(start, end)
+                .or(vacation.endDateTime.time.between(start, end)))
+            .orderBy(vacation.startDateTime.time.asc())
             .fetch();
+
+        return Vacations.of(fetch);
     }
 
 }
--- src/main/java/com/sixshop/sixspace/vacation/service/VacationService.java
+++ src/main/java/com/sixshop/sixspace/vacation/service/VacationService.java
@@ -1,12 +1,16 @@
 package com.sixshop.sixspace.vacation.service;
 
+import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
 import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
 
 import com.sixshop.sixspace.vacation.domain.Vacation;
+import com.sixshop.sixspace.vacation.domain.Vacations;
 import com.sixshop.sixspace.vacation.repository.VacationRepository;
 import com.sixshop.sixspace.vacation.repository.VacationRepository2;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +23,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class VacationService {
 
+    private static final int WEEK_DAYS = 7;
     private static final int LUNCH_HOUR = 13;
     private static final int FINISH_WORK_HOUR = 18;
+    private static final LocalTime TODAY_LAST_TIME = LocalTime.of(23, 59);
     private final VacationRepository vacationRepository;
     private final VacationRepository2 vacationRepository2;
 
@@ -28,11 +34,12 @@ public class VacationService {
     public List<Vacation> findAllByBetweenDates(final int year, final int month) {
         final LocalDateTime startDayOfMonth = LocalDateTime.of(year, month, 1, 0, 0);
         final LocalDateTime endDayOfMonth = startDayOfMonth.with(lastDayOfMonth())
-                                                           .withHour(23)
-                                                           .withMinute(59);
+            .withHour(23)
+            .withMinute(59);
 
 //        return vacationRepository.findAllByBetweenDates(startDayOfMonth, endDayOfMonth);
-        return vacationRepository2.findAllByBetweenDates(startDayOfMonth, endDayOfMonth);
+        return vacationRepository2.findAllByBetweenDates(startDayOfMonth, endDayOfMonth)
+            .getVacations();
     }
 
     public void getVacationsOfMonth(final int year, final int month) {
@@ -47,17 +54,18 @@ public class VacationService {
      * 3. 중복 제거하면서 같은날이면 시간 합쳐서 연차인지 시간휴가인지 계산
      * 4. 테스트 코드 작성
      */
-    private List<FilteredVacation> filterVacationOfMonth(final int year, final int month, final List<Vacation> vacations) {
+    private List<FilteredVacation> filterVacationOfMonth(final int year, final int month,
+        final List<Vacation> vacations) {
         final LocalDate startDayOfMonth = LocalDate.of(year, month, 1);
         final LocalDate endDayOfMonth = startDayOfMonth.with(lastDayOfMonth());
         final List<FilteredVacation> filtered = new ArrayList<>();
 
         for (final Vacation vacation : vacations) {
-            final LocalDate from = vacation.getStartDateTime().toLocalDate();
-            final LocalDate to = vacation.getEndDateTime().toLocalDate();
+            final LocalDate from = vacation.getStartDateTime().getTime().toLocalDate();
+            final LocalDate to = vacation.getEndDateTime().getTime().toLocalDate();
 
             int totalUseHour = vacation.getUseHour();
-            int startHour = vacation.getStartDateTime().getHour();
+            int startHour = vacation.getStartDateTime().getTime().getHour();
 
             LocalDate cursor = from;
 
@@ -90,4 +98,38 @@ public class VacationService {
         }
         return FINISH_WORK_HOUR - startHour;
     }
+
+    public void getAboutMonthDuration(String userId) {
+        LocalDate now = LocalDate.now();
+        LocalDate lastDay = getAboutMonthStartDate(now);
+        LocalDate firstDay = getAboutMonthEndDate(now);
+
+        Vacations vacations = vacationRepository2.findAllByBetweenDates(
+            LocalDateTime.of(firstDay, LocalTime.MIDNIGHT),
+            LocalDateTime.of(lastDay, TODAY_LAST_TIME));
+
+        int weekUseVacationHour = vacations.bringWeekUseVacationHour(now);
+        int monthUseVacationHour = vacations.bringMonthVacationHour(now);
+    }
+
+    private LocalDate getAboutMonthStartDate(LocalDate now) {
+        LocalDate lastDay = now.with(lastDayOfMonth());
+
+        if (ChronoUnit.DAYS.between(now, lastDay) < WEEK_DAYS) {
+            lastDay = now.plusDays(WEEK_DAYS);
+        }
+
+        return lastDay;
+    }
+
+    private LocalDate getAboutMonthEndDate(LocalDate now) {
+        LocalDate firstDay = now.with(firstDayOfMonth());
+
+        if (ChronoUnit.DAYS.between(now, firstDay) < WEEK_DAYS) {
+            firstDay = now.minusDays(WEEK_DAYS);
+        }
+
+        return firstDay;
+    }
+
 }
