7089aa28c6796b36236eb88c9e7b4221b6a6c86e
00hongjun <00hongjun@gmail.com>
2021-06-19 16:44:11 +0900

    <feat> 휴가 추가, 수정 기능 추가

--- src/main/java/com/sixshop/sixspace/vacation/presentation/VacationController.java
+++ src/main/java/com/sixshop/sixspace/vacation/presentation/VacationController.java
@@ -3,12 +3,18 @@ package com.sixshop.sixspace.vacation.presentation;
 import com.sixshop.sixspace.vacation.presentation.dto.ApiResponse;
 import com.sixshop.sixspace.vacation.presentation.dto.DayOfMonthVacationResponse;
 import com.sixshop.sixspace.vacation.presentation.dto.UserMonthlyStatisticsResponse;
+import com.sixshop.sixspace.vacation.presentation.dto.VacationCreateRequest;
+import com.sixshop.sixspace.vacation.presentation.dto.VacationUpdateRequest;
 import com.sixshop.sixspace.vacation.service.DayOfMonthVacationService;
 import com.sixshop.sixspace.vacation.service.VacationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +42,19 @@ public class VacationController {
         ApiResponse<UserMonthlyStatisticsResponse> result = new ApiResponse<>(
             response);
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping
+    public ResponseEntity createVacation(@RequestBody VacationCreateRequest request) {
+        vacationService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .build();
+    }
+
+    @PutMapping
+    public ResponseEntity updateVacation(@RequestBody VacationUpdateRequest request) {
+        vacationService.update(request);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
