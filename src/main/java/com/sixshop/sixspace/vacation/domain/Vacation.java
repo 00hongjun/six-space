c5fb2b38e65466686b7b6b75923fc1912c99b98a
00hongjun <00hongjun@gmail.com>
2021-06-20 00:42:38 +0900

    <fix> 연차 판단 관련 메서드 추가

--- src/main/java/com/sixshop/sixspace/vacation/domain/Vacation.java
+++ src/main/java/com/sixshop/sixspace/vacation/domain/Vacation.java
@@ -1,5 +1,6 @@
 package com.sixshop.sixspace.vacation.domain;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
@@ -48,6 +49,20 @@ public class Vacation {
 
     public LocalDateTime getEndDateTimeValue() {
         return endDateTime.getTime();
+    }
+
+    public boolean isDailyVacation() {
+        return startDateTime.isStartGoOfficeTime() && endDateTime.isEndLeaveOfficeTime();
+    }
+
+    public boolean isIncludeDatePeriod(LocalDate localDate) {
+        LocalDate start = getStartDateTimeValue()
+            .toLocalDate();
+        LocalDate end = getEndDateTimeValue()
+            .toLocalDate();
+
+        return (start.isBefore(localDate) || start.equals(localDate))
+            && (end.isAfter(localDate) || end.isEqual(localDate));
     }
 
 }
