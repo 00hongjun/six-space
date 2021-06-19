b1bb05b2a45d623b703f8567e8cfaa82e4a6eea9
00hongjun <00hongjun@gmail.com>
2021-06-19 16:43:13 +0900

    <docs> repository Deprecated 처리
    - repository 하나로

--- src/main/java/com/sixshop/sixspace/vacation/repository/DayOfMonthVacationRepository.java
+++ src/main/java/com/sixshop/sixspace/vacation/repository/DayOfMonthVacationRepository.java
@@ -4,7 +4,6 @@ import static com.sixshop.sixspace.vacation.domain.QVacation.vacation;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sixshop.sixspace.vacation.domain.Vacation;
-import com.sixshop.sixspace.vacation.domain.Vacations;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +11,15 @@ import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
 @Repository
+@Deprecated
 public class DayOfMonthVacationRepository {
 
     private final JPAQueryFactory queryFactory;
 
+    /**
+     * {@link VacationRepository#findAllByBetweenDates}
+     */
+    @Deprecated
     public List<Vacation> findAllByBetweenDates(LocalDateTime start, final LocalDateTime end) {
         return queryFactory
             .select(vacation)
@@ -26,6 +30,10 @@ public class DayOfMonthVacationRepository {
             .fetch();
     }
 
+    /**
+     * {@link VacationRepository#findAllByUserId}
+     */
+    @Deprecated
     public List<Vacation> findAllByUserId(String userId) {
         return queryFactory
             .select(vacation)
