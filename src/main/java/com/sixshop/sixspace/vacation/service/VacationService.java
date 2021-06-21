2b41c446af055fd6f4f9e5c56e2f61495a4f3245
hongjun <00hongjun@gmail.com>
2021-06-21 15:53:55 +0900

    <feat> 휴가 추가 수정
    - repository 하나로
    - 휴가 추가, 수정 기능 추가

--- src/main/java/com/sixshop/sixspace/vacation/service/VacationService.java
+++ src/main/java/com/sixshop/sixspace/vacation/service/VacationService.java
@@ -7,9 +7,14 @@ import com.sixshop.sixspace.exception.ErrorCode;
 import com.sixshop.sixspace.exception.ServiceException;
 import com.sixshop.sixspace.user.domain.User;
 import com.sixshop.sixspace.user.infrastructure.UserRepository;
+import com.sixshop.sixspace.vacation.domain.Vacation;
+import com.sixshop.sixspace.vacation.domain.VacationLocalDateTime;
 import com.sixshop.sixspace.vacation.domain.Vacations;
 import com.sixshop.sixspace.vacation.presentation.dto.UserMonthlyStatisticsResponse;
+import com.sixshop.sixspace.vacation.presentation.dto.VacationCreateRequest;
+import com.sixshop.sixspace.vacation.presentation.dto.VacationUpdateRequest;
 import com.sixshop.sixspace.vacation.repository.DayOfMonthVacationRepository;
+import com.sixshop.sixspace.vacation.repository.VacationRepository;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,18 +22,41 @@ import java.time.LocalTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class VacationService {
 
     private static final int WEEK_DAYS = 7;
     private static final int LUNCH_HOUR = 13;
     private static final int FINISH_WORK_HOUR = 18;
     private static final LocalTime TODAY_LAST_TIME = LocalTime.of(23, 59);
-    private final DayOfMonthVacationRepository vacationQueryRepository;
+    private final DayOfMonthVacationRepository dayOfMonthVacationRepository;
+    private final VacationRepository vacationRepository;
     private final UserRepository userRepository;
+
+    public Vacation findVacation(int vacationId) {
+        return vacationRepository.findById(vacationId)
+            .orElseThrow(() -> new IllegalArgumentException("휴가 조회 실패"));
+    }
+
+    @Transactional
+    public void create(VacationCreateRequest request) {
+        Vacation vacation = new Vacation(request.getUserId(), VacationLocalDateTime.of(request.getStartTime()),
+            request.getUseHour());
+        vacation.updateReason(request.getReason());
+        vacationRepository.save(vacation);
+    }
+
+    @Transactional
+    public void update(VacationUpdateRequest request) {
+        Vacation vacation = findVacation(request.getVacationId());
+        vacation.updateStartDateTime(VacationLocalDateTime.of(request.getStartTime()), request.getUseHour());
+        vacation.updateReason(request.getReason());
+    }
 
     /**
      * TODO - 다가오는 휴가는 무제한으로 모두 조회 해야 함
@@ -37,7 +65,7 @@ public class VacationService {
         LocalDate now = LocalDate.now();
         User user = userRepository.findById(userId)
             .orElseThrow(() -> new ServiceException(ErrorCode.NOT_FOUND_USER));
-        Vacations vacations = Vacations.of(vacationQueryRepository.findAllByUserId(userId));
+        Vacations vacations = Vacations.of(dayOfMonthVacationRepository.findAllByUserId(userId));
 
         int weekUseVacationHour = vacations
             .bringVacationByRange(bringWeekStartDateTime(now), bringWeekEndDateTime(now))
