8a177a8ee97e6dbb2ec7814008b8e14c19453e06
ohtaeg <otk1090@naver.com>
2021-07-04 00:15:27 +0900

    <feat> : 슬랙 id로 유저 조회 기능 추가 및 테스트

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,36 +1,18 @@
 INSERT INTO user (id, email, join_date, name, nick_name, password, resignation_date, slack_id, total_vacation_time)
-VALUES (1, '00hongjun@sixshop.com', '2021-02-01', '최홍준', '이태성', '1234', null, 'qwer', 15);
+VALUES (1, '00hongjun@sixshop.com', '2021-02-01', '최홍준', '이태오', '1234', null, 'U01NUKKL074', 15);
 
 INSERT INTO user (id, email, join_date, name, nick_name, password, resignation_date, slack_id, total_vacation_time)
-VALUES (2, 'ohtaeg@sixshop.com', '2021-04-01', '오태경2', '태태로2', '1234', null, 'asdf', 15);
-
-INSERT INTO user (id, email, join_date, name, nick_name, password, resignation_date, slack_id, total_vacation_time)
-VALUES (3, 'ohtaeg@sixshop.com', '2021-04-01', '오태경3', '태태로3', '1234', null, 'asdf', 15);
+VALUES (2, 'ohtaeg@sixshop.com', '2021-04-01', '오태경', '태태로', '1234', null, 'U01SXH2CRT7', 15);
 
 -- vacation
 INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (11, '2021-05-31 09:00', '2021-06-01 18:00', 'BASIC', '개인정비1', 16, 1);
+VALUES (1, '2021-05-31 09:00', '2021-06-01 18:00', 'BASIC', '개인정비1', 16, 1);
 
 INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (12, '2021-06-02 09:00', '2021-06-02 10:00', 'BASIC', '개인정비2', 1, 1);
+VALUES (2, '2021-06-02 09:00', '2021-06-02 10:00', 'BASIC', '개인정비2', 1, 1);
 
 INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (13, '2021-05-31 14:00', '2021-06-02 13:00', 'BASIC', '개인정비3', 16, 2);
+VALUES (3, '2021-05-31 14:00', '2021-06-02 13:00', 'BASIC', '개인정비3', 16, 2);
 
 INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (14, '2021-06-03 14:00', '2021-06-04 13:00', 'BASIC', '개인정비4', 8, 2);
-
-INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (15, '2021-06-01 10:00', '2021-06-01 18:00', 'BASIC', '개인정비5', 7, 3);
-
-INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (16, '2021-06-29 09:00', '2021-07-02 18:00', 'BASIC', '개인정비6', 32, 3);
-
-INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (17, '2021-06-04 09:00', '2021-06-04 10:00', 'BASIC', '개인정비7', 1, 3);
-
-INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (18, '2021-06-04 10:00', '2021-06-04 11:00', 'BASIC', '개인정비8', 1, 3);
-
-INSERT INTO vacation (id, start_date_time, end_date_time, type, reason, use_hour, user_id)
-VALUES (19, '2021-06-11 12:00', '2021-06-12 11:00', 'BASIC', '개인정비9', 7, 4);
+VALUES (4, '2021-06-03 14:00', '2021-06-04 13:00', 'BASIC', '개인정비4', 8, 2);
--- src/test/java/com/sixshop/sixspace/user/infrastructure/UserRepositoryTest.java
+++ src/test/java/com/sixshop/sixspace/user/infrastructure/UserRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.sixshop.sixspace.user.infrastructure;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.sixshop.sixspace.user.domain.User;
 import com.sixshop.sixspace.user.presentation.dto.UserCreateRequest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -40,7 +41,31 @@ class UserRepositoryTest {
             .get();
 
         // then
-        Assertions.assertThat(user).isEqualTo(target);
+        assertThat(user).isEqualTo(target);
+    }
+
+    @DisplayName("슬랙 ID로 유저를 조회할 수 있다.")
+    @Test
+    void findBySlackId() {
+        // given
+        final String slackId = "U01SXH2CRT7";
+
+        // when
+        User actual = userRepository.findBySlackId(slackId)
+                                    .get();
+
+        // then
+        assertThat(actual.getSlackId()).isEqualTo(slackId);
+    }
+
+    @DisplayName("없는 슬랙 ID로 유저를 조회할 경우 예외 발생을 성공한다.")
+    @Test
+    void findBySlackId_exception() {
+        // given
+        final String slackId = "asdf";
+
+        // when then
+        assertThatThrownBy(() -> userRepository.findBySlackId(slackId).get());
     }
 
 }
